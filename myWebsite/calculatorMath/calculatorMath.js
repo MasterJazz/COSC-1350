766edd32da5c0e04bc7c79511dd45a871b4aba3d
Matthew <matthewsleight@westernwyoming.edu>
2024-10-13 20:37:40 -0600

    Add files via upload

--- myWebsite/calculatorMath/calculatorMath.js
+++ myWebsite/calculatorMath/calculatorMath.js
@@ -6,28 +6,37 @@
 
 function main() {
     /* Get the value of the bill amount from the input field */
-    const theBillAmount = document.getElementByld("billAmount").value;
+    const theBillAmount = document.getElementById("billAmount").value;
     /* Get the value of the tip percentage from the input field */
-    const theTipPercentage = document.getElementByld("tipPercentage").value;
+    const theTipPercentage = document.getElementById("tipPercentage").value;
+
+    /* Convert the inputs to numbers */
+    const billAmount = parseFloat(theBillAmount);
+    const tipPercentage = parseFloat(theTipPercentage);
+
+
+    // // Troubleshooting / Debugging
+    // console.log("Bill Amount (type): ", typeof billAmount);
+    // console.log("Tip Percentage (type): ", typeof tipPercentage);
+
 
     /* Call the function to calculate the tip based on user input */
-    calculate(theBillAmount, theTipPercentage);
+    calculate(billAmount, tipPercentage);
 }
 
-function calculate(theBillAmount, tipPercentage) {
+function calculate(theBillAmount, theTipPercentage) {
     /* Validate the user input */
-    if (theBillAmount == 0 || theBillAmount == String) {
+    if (theBillAmount <= 0) {
         alert("Your input was invalid.");
         return main;
     }
     else {
         /* Calculate tip */
-        let tip = (theBillAmount * tipPercentage) / 100;
+        let tip = theBillAmount * theTipPercentage;
 
         /* Calculate total amount */
         let total = theBillAmount + tip;
 
-        calculate.innerHTML = "The calculated tip was: " + total;
-        return "Tip: " + tip + "Total: " + total;
+        document.getElementById("result").innerHTML = "Tip: $" + tip + " | Total: $" + total;
     }
 }
