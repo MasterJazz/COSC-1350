315aee1891d838435f6833f6c94943abaee25672
Matthew <matthewsleight@westernwyoming.edu>
2024-10-21 18:36:25 -0600

    Add files via upload

---
+++ m09/js03_txt.js
@@ -0,0 +1,11 @@
+/*
+	Tipton Turbines
+     Program to display games results in a web table
+     Author: Matthew Sleight
+     Date: October 21, 2024
+
+     Filename: js03.js
+ */
+
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
